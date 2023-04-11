09ce07a4dbf56710c31560561b9175aaf2a1188a
Adam Gonzales <adamgonzales1@gmail.com>
2023-04-11 11:07:08 -0600

    feature: add README.md, log into website

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pyjob Seeker 
+
+Searches LinkedIn for jobs based on selected parameters. 
+
+[//]: # (Check it out: [Quizzical]&#40;https://quizzical-alpha.vercel.app/&#41;)
+
+## About this Project
+
+**Technologies Used:** Python, Selenium
+
+[//]: # (Deployed using [Vercel]&#40;https://vercel.com/&#41;)
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hi from pyjob seeker")
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+LINKEDIN_URL = "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
+PYTHON_JOB_SEARCH_QUERY = "https://www.linkedin.com/jobs/search/?currentJobId=3550859748&f_AL=true&f_WT=2&geoId=103644278&keywords=python%20developer&location=United%20States&refresh=true&sortBy=DD"
+LINKEDIN_USERNAME = os.environ.get("LINKEDIN_USERNAME")
+LINKEDIN_PASSWORD = os.environ.get("LINKEDIN_PASSWORD")
+
+driver = webdriver.Chrome()
+driver.get(LINKEDIN_URL)
+
+username_field = driver.find_element(By.ID, "username")
+password_field = driver.find_element(By.ID, "password")
+login_button = driver.find_element(By.CSS_SELECTOR, "button")
+
+username_field.send_keys(LINKEDIN_USERNAME)
+password_field.send_keys(LINKEDIN_PASSWORD)
+login_button.click()
+
+# the following line keeps the browser open after the script runs
+keyword = input("enter a character or press enter to continue")
+
+driver.quit()
